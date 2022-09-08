ee5f548f0ceeed81c5e6ee076c3a6ddf76c952c8
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-09-08 19:31:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
-import { getPost } from './controllers/PostController';
 
 dotenv.config();
 const app = express();
@@ -19,30 +18,6 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
 
 app.use('/', routes);
-
-app.get('/*', (req, res, next) => {
-  fs.readFile(indexPath, 'utf8', (err, htmlData) => {
-    if (err) {
-      console.error('Error during file reading', err);
-      return res.status(404).end()
-    }
-    
-    const postId = req.query.id;
-    const post = getPost(postId);
-    if(!post) return res.status(404).send("Post not found");
-
-    htmlData = htmlData.replace(
-      "<title>Tech Blog</title>",
-      `<title>${post.postTitle}</title>`
-    )
-    .replace('__META_OG_TITLE__', post.postTitle)
-    .replace('__META_OG_DESCRIPTION__', post.content.slice(0, 100))
-    .replace('__META_DESCRIPTION__', post.content.slice(0, 100))
-    .replace('__META_OG_IMAGE__', post.thumbnail)
-    
-    return res.send(htmlData);
-  });
-});
 
 mongoose.connect(   
   process.env.NODE_ENV_MONGO_KEY, {
